caef33225ced1f5bdce4d502faea96f8f787fcc2
fikreanteneh <fikre.anteneh01@gmail>
2024-07-22 22:41:32 +0300

    experience setup

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,11 +2,16 @@
 import React from 'react'
 import { ExperienceModel } from '../types/experience.model'
 
-
-const Experience = ({ experience: ExperienceModel }) => {
+const Experience = (props) => {
+    const experience: ExperienceModel = props.experience
     return (
         <div>
             <p>{experience.StartDate} - {experience.EndDate}</p>
+            <div>
+                <img src={experience.Image} alt={experience.Company} />
+            </div>
+            <h3>{experience.Company}</h3>
+            <h4>{experience.Title}</h4>
         </div>
     )
 }
