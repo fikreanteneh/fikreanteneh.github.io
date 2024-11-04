ec33d2ea25fad7924979cf7fde9344e82f97d4dd
fikreanteneh <fikre.anteneh01@gmail>
2024-11-04 15:54:20 +0300

    fix: import fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from './ThemeProvider';
 import Home from './layouts/Home';
@@ -27,7 +26,6 @@ function App() {
   return (
     <ThemeProvider >
       <RouterProvider router={Router} />
-      <ToastContainer autoClose={500} />
     </ThemeProvider>
   )
 }
